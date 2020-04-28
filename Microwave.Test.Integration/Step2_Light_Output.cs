81bd74dcea9c548b8b2d9476c86f90ebb3957f6d
MagmaMusen <MagmaMusen@gmail.com>
2020-04-28 21:37:22 +0200

    Some unfinished tests

---
+++ Microwave.Test.Integration/Step2_Light_Output.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    class Step2_Light_Output
+    {
+        private IOutput _output;
+        private ILight _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+                _output = new Output();
+                _uut = new Light(_output);
+        }
+    }
+}
---
+++ Microwave.Test.Integration/Step5_UserInterface_Display.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    class Step5_UserInterface_Display
+    {
+        private IOutput _output;
+        private ICookController _cookController;
+        private IDoor _door;
+        private IButton _button;
+        private IUserInterface _uut;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _output = new Substitute.For(IOutput);
+            
+        }
+    }
+}
