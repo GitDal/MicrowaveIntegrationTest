60b40ee96e15cadfe38450e13e0d0e2d0c22b5ba
GitDal <jeppedybdal@gmail.com>
2020-04-28 21:29:58 +0200

    Step4 Og en midlertidig ikke implimenteret STEP 3 OKAY MIKKEL

---
+++ MicrowaveOvenClasses/Boundary/Step3_PowerTube_Output.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    class Step3_PowerTube_Output
+    {
+    }
+}
