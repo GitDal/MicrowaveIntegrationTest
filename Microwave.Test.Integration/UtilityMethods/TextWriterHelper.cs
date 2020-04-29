dd60cb481fd7ac4bfc54c579efd4a8c4dc7cc23e
Mikkel-KR <Mikkel.kora@gmail.com>
2020-04-29 15:16:55 +0200

    Made utility method for textWriter and made step 2 and 3 done. Started on Step 6

---
+++ Microwave.Test.Integration/UtilityMethods/TextWriterHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microwave.Test.Integration.UtilityMethods
+{
+    public static class TextWriterHelper
+    {
+        // Utility method
+        public static void ClearTextWriter(StringWriter tr)
+        {
+            StringBuilder sb = tr.GetStringBuilder();
+            sb.Remove(0, sb.Length);
+        }
+    }
+}
