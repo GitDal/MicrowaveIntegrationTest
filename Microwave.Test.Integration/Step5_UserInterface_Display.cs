92f433b2f6374f1a8acea43d93a06de67d908001
MagmaMusen <MagmaMusen@gmail.com>
2020-04-29 13:25:56 +0200

    Updated step 5

--- Microwave.Test.Integration/Step5_UserInterface_Display.cs
+++ Microwave.Test.Integration/Step5_UserInterface_Display.cs
@@ -4,26 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
 using NSubstitute;
 using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
+    [TestFixture]
     class Step5_UserInterface_Display
     {
         private IOutput _output;
         private ICookController _cookController;
         private IDoor _door;
-        private IButton _button;
-        private IUserInterface _uut;
-
+        private IButton _powerButton, _timeButton, _startCancelButton;
+        private ILight _light;
+        private IDisplay _display;
+        private IUserInterface _tlm;
 
         [SetUp]
         public void SetUp()
         {
-            //_output = new Substitute.For(IOutput);
-            
+            // Stubs, mocks, fakes.
+            _output = Substitute.For<IOutput>();
+            _light = Substitute.For<ILight>();
+            _cookController = Substitute.For<ICookController>();
+            _door = Substitute.For<IDoor>();
+            _powerButton = Substitute.For<IButton>();
+            _timeButton = Substitute.For<IButton>();
+            _startCancelButton = Substitute.For<IButton>();
+
+
+            // Included.
+            _display = new Display(_output);
+            _tlm = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
         }
+
+        [Test]
+        public void ready_2PowerButton_PowerIs100()
+        {
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            _display.Received(1).ShowPower(Arg.Is<int>(100));
+        }
+
+
+
     }
+
 }
